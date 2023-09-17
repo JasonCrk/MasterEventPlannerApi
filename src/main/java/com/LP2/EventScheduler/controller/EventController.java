337f94a068fdff5e14b073d5b0c808b02db31426
JasonCrk <emerzonkolki@gmail.com>
2023-09-16 19:52:14 -0500

    feat: search public events endpoint base

--- src/main/java/com/LP2/EventScheduler/controller/EventController.java
+++ src/main/java/com/LP2/EventScheduler/controller/EventController.java
@@ -1,12 +1,24 @@
 package com.LP2.EventScheduler.controller;
 
+import com.LP2.EventScheduler.service.event.EventService;
+
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/events")
 @RequiredArgsConstructor
 public class EventController {
+
+    private final EventService eventService;
+
+    @GetMapping(path = "/seach")
+    public List<?> searchPublicEvents() {
+        return eventService.searchPublicEvents();
+    }
 }
---
+++ src/main/java/com/LP2/EventScheduler/service/event/EventServiceImpl.java
@@ -0,0 +1,16 @@
+package com.LP2.EventScheduler.service.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceImpl implements EventService {
+
+    @Override
+    public List<?> searchPublicEvents() {
+        return null;
+    }
+}
