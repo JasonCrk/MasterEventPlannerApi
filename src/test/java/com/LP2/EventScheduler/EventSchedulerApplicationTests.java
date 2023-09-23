76bc4b32062b1c1c8405bcc3a1cef344a8f4545c
JasonCrk <emerzonkolki@gmail.com>
2023-09-23 18:28:42 -0500

    test: update EventSchedulerApplicationTests

--- src/test/java/com/LP2/EventScheduler/EventSchedulerApplicationTests.java
+++ src/test/java/com/LP2/EventScheduler/EventSchedulerApplicationTests.java
@@ -1,9 +1,17 @@
 package com.LP2.EventScheduler;
 
+import jakarta.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@Profile("test")
+@Transactional
+@ExtendWith(SpringExtension.class)
 class EventSchedulerApplicationTests {
 
 	@Test
