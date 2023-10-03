a2fd3196d86daa8f61c08007386ef8705d04e79a
JasonCrk <emerzonkolki@gmail.com>
2023-10-03 13:41:28 -0500

    refactor(SecurityConfig.java): remove imports

--- src/main/java/com/LP2/EventScheduler/security/SecurityConfig.java
+++ src/main/java/com/LP2/EventScheduler/security/SecurityConfig.java
@@ -8,13 +8,11 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -51,5 +49,4 @@ public class SecurityConfig {
                 )
                 .build();
     }
-
 }
