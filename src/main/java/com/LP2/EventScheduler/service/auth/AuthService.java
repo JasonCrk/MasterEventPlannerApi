3227482546708628c5a4c4da32bccfdbc77f231e
JasonCrk <emerzonkolki@gmail.com>
2023-10-19 16:20:21 -0500

    feat: retrieve user by token endpoint

--- src/main/java/com/LP2/EventScheduler/service/auth/AuthService.java
+++ src/main/java/com/LP2/EventScheduler/service/auth/AuthService.java
@@ -2,12 +2,15 @@ package com.LP2.EventScheduler.service.auth;
 
 import com.LP2.EventScheduler.dto.auth.LoginDTO;
 import com.LP2.EventScheduler.dto.auth.RegisterDTO;
+import com.LP2.EventScheduler.model.User;
 import com.LP2.EventScheduler.response.auth.JwtResponse;
+import com.LP2.EventScheduler.response.user.SimpleUserResponse;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthService {
+    SimpleUserResponse retrieveUserByToken(User authUser);
     JwtResponse register(RegisterDTO registerData);
     JwtResponse login(LoginDTO loginData);
     JwtResponse refreshToken(HttpServletRequest request, HttpServletResponse response);
