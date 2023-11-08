6c35e23919b3538e1dcbaefcf6b51ed73fef41a7
JasonCrk <emerzonkolki@gmail.com>
2023-11-07 21:06:12 -0500

    fix(UserMapper.java): use userName

--- src/main/java/com/LP2/EventScheduler/response/account/AccountMapper.java
+++ src/main/java/com/LP2/EventScheduler/response/account/AccountMapper.java
@@ -2,6 +2,7 @@ package com.LP2.EventScheduler.response.account;
 
 import com.LP2.EventScheduler.model.Account;
 import com.LP2.EventScheduler.model.Invitation;
+import com.LP2.EventScheduler.response.user.UserMapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,7 +10,7 @@ import org.mapstruct.factory.Mappers;
 
 import java.util.Optional;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface AccountMapper {
 
     AccountMapper INSTANCE = Mappers.getMapper(AccountMapper.class);
--- src/main/java/com/LP2/EventScheduler/response/user/UserMapper.java
+++ src/main/java/com/LP2/EventScheduler/response/user/UserMapper.java
@@ -3,6 +3,7 @@ package com.LP2.EventScheduler.response.user;
 import com.LP2.EventScheduler.model.User;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -10,5 +11,9 @@ public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(source = "userName", target = "username")
     SimpleUserResponse toSimpleResponse(User user);
+
+    @Mapping(source = "userName", target = "username")
+    UserUsernameResponse toUsernameResponse(User user);
 }
