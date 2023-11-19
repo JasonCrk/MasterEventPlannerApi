e20a5545bfe031cdc638a9d61b940f15638c7432
JasonCrk <emerzonkolki@gmail.com>
2023-11-19 18:24:51 -0500

    fix(EventSchedulerApplication.java): import EnableAsync

--- src/main/java/com/LP2/EventScheduler/EventSchedulerApplication.java
+++ src/main/java/com/LP2/EventScheduler/EventSchedulerApplication.java
@@ -2,6 +2,8 @@ package com.LP2.EventScheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
